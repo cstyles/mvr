44683ee2bfc78e6ab8f31af8997bdd5d51b184f6
Collin Styles <collingstyles@gmail.com>
2016-06-27 13:05:32 -0700

    Removed counter code and comments

--- mvr.py
+++ mvr.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 
-####
-# mvr
-# A wrapper for mv that enables usage of regular expressions.
-# SYNTAX: mvr GLOB_PATTERN MATCH_REGEX REPLACE_REGEX OPTIONS
-# - GLOB_PATTERN: A string like '*.txt'.
-# - MATCH_REGEX: A regex that decides which files to rename.
-# - REPLACE_REGEX: A regex that decides how to rename the files.
-# - OPTIONS: Things like full-match mode, etc.
-####
-
-# TODO:
-# - Add modes ('m' for match; 's' for substitute)
-# - Add switch for full-match mode
-# - Add counter string (i.e., '{0}' in replace_regex maps to an increasing counter)
-
 import re
 import os
 import sys
@@ -69,16 +54,12 @@ def mvr(argv):
         exit(1)
     
     # Rename the files
-    # counter = 0
     for old, new in zip(args.files, new_files):
         if old == new:
             continue
         
-        # TODO: zfill with log(len(new_files)) or something
-        # new.format(counter) # counter feature
         print('"{0}" => "{1}"'.format(old, new))
         os.rename(old, new)
-        # counter += 1
 
 # Main method
 if __name__ == '__main__':
